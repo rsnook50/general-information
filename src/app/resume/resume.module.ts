5b1e213a56dec14c3b2d687f344597d94b859146
Richard J. Snook <rsnook50@gmail.com>
2022-07-13 13:19:13 -0400

    Added resume module and declared resume component

---
+++ src/app/resume/resume.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from "@angular/core";
+import { ResumeComponent } from "./resume.component";
+
+@NgModule({
+    declarations: [
+        ResumeComponent
+    ],
+    imports: [],
+    exports: []
+})
+
+export class ResumeModule {}
