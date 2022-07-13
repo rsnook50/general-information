2c053dbe463fd492016d9be3d13dc91d21a43e4a
Richard J. Snook <rsnook50@gmail.com>
2022-07-13 13:09:54 -0400

    added resume routing module

---
+++ src/app/resume/resume.component.ts
@@ -0,0 +1,8 @@
+import { Component } from "@angular/core";
+
+@Component({
+    templateUrl: "./resume.component.html",
+    styleUrls: ["./resume.component.css"]
+})
+
+export class ResumeComponent {}
---
+++ src/app/resume/resume.routing.module.ts
@@ -0,0 +1,7 @@
+import { Routes } from "@angular/router";
+
+export const ResumeRoutes: Routes = [
+    {
+        path: "resume"
+    }
+]
