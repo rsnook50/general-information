44e4e6cf7af476fb90c0dc5dee0d627d8ae20253
Richard J. Snook <rsnook50@gmail.com>
2022-07-13 14:53:08 -0400

    Added title and description

--- src/app/resume/resume.component.ts
+++ src/app/resume/resume.component.ts
@@ -5,4 +5,7 @@ import { Component } from "@angular/core";
     styleUrls: ["./resume.component.css"]
 })
 
-export class ResumeComponent {}
+export class ResumeComponent {
+    title: string = "Richard J. Snook";
+    description: string = "Result-driven with 10+ years of experience building platform and mobile products and 3+ years experience working in an agile environment.  Proven ability to effectively lead cross-functional teams toward expanded products and services. Proven track record in taking networking software products to market, conceptualization, design, and development of software systems. Extensive knowledge of networking across hardware, firmware, and software applications.";
+}
