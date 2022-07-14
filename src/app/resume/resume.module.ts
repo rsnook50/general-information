a1b6327e7657709952f392ba228bd910b9cb5c77
Richard J. Snook <rsnook50@gmail.com>
2022-07-13 23:38:52 -0400

    Added Opportunity Summary to Resume

--- src/app/resume/resume.module.ts
+++ src/app/resume/resume.module.ts
@@ -1,15 +1,19 @@
+import { CommonModule } from "@angular/common";
 import { NgModule } from "@angular/core";
 
 import { ContactComponent } from "../contact/contact.component";
+import { OpportunitySummaryComponent } from "../opportunity/opportunity-summary.component";
 import { ResumeComponent } from "./resume.component";
 import { ResumeRoutingModule } from "./resume.routing.module";
 
 @NgModule({
     declarations: [
         ResumeComponent,
-        ContactComponent
+        ContactComponent,
+        OpportunitySummaryComponent
     ],
     imports: [
+        CommonModule,
         ResumeRoutingModule
     ],
     exports: [
