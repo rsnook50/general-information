9d306b23bb81e56984961173a66c966560df6799
Richard J. Snook <rsnook50@gmail.com>
2022-07-20 17:12:41 -0400

    Updated welcome page and some other stuff to go with it.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,6 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NavigationListComponent } from './navigation/navigation-list.component';
 import { WelcomeComponent } from './home/welcome.component';
 import { HttpClientModule } from '@angular/common/http';
 import { DefaultLayoutComponent } from './_layouts/default-layout.component';
--- src/app/home/welcome.component.ts
+++ src/app/home/welcome.component.ts
@@ -1,9 +1,16 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-    templateUrl: "./welcome.component.html"
+    templateUrl: "./welcome.component.html",
+    styleUrls: ["./welcome.component.css"]
 })
 
-export class WelcomeComponent {
-    public pageTitle = "Welcome";
+export class WelcomeComponent implements OnInit{
+    developYears: number = 0;
+
+    ngOnInit(): void {
+        var startYear = 2007;
+        var currentYear = new Date().getFullYear();
+        this.developYears = currentYear - startYear;
+    }
 }
